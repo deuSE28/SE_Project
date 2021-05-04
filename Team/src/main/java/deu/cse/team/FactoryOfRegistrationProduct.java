1126a4a848a5da3d2dc8e079eabe4574155c0b76
kimhyoju <73176190+kimhyoju123@users.noreply.github.com>
2021-05-04 11:12:17 +0900

    Add files via upload

---
+++ Team/src/main/java/deu/cse/team/FactoryOfRegistrationProduct.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deu.cse.team;
+
+/**
+ *
+ * @author qjqmf
+ */
+public class FactoryOfRegistrationProduct {
+    public void createProduct(String type) {
+        RegistrationProduct_Interface re_interface = null;
+        switch(type){
+            case "Cloth":
+                re_interface = new Kategorie_Cloth();
+            
+            case "Game" : 
+                re_interface = new Kategorie_Game();
+                break;
+        }
+        
+        re_interface.createPrice();
+        re_interface.createState();
+    }
+}
---
+++ Team/src/main/java/deu/cse/team/RegistrationProduct_Interface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deu.cse.team;
+
+/**
+ *
+ * @author qjqmf
+ */
+public interface RegistrationProduct_Interface {
+    public void createPrice();
+    public void createState();
+}
