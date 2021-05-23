f7b9cdf3b237908907178d8afe31ba66d780bf83
TTANGGIN <knm6175@gmail.com>
2021-05-24 00:03:29 +0900

    콤보박스 기본값('없음') 추가

--- Team/src/main/java/deu/cse/team/search/Search.java
+++ Team/src/main/java/deu/cse/team/search/Search.java
@@ -30,6 +30,7 @@ public class Search extends javax.swing.JFrame {
     public void cmbList() {
         DefaultComboBoxModel model = new DefaultComboBoxModel<>();
         BufferedReader br = null;
+        model.addElement("없음");
         try {
             br = new BufferedReader(new FileReader("C:\\DB\\category.txt"));
             int val = Integer.parseInt(br.readLine());
@@ -82,7 +83,7 @@ public class Search extends javax.swing.JFrame {
 
         jLabel5.setText("거래지역 :");
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "세종특별자치시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도" }));
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "없음", "서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "세종특별자치시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도" }));
 
         searchButton.setText("검색");
         searchButton.addActionListener(new java.awt.event.ActionListener() {
