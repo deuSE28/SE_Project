96185490dcfe6d5d8397455d29fe77bc37ddd583
구본영 <@DESKTOP-1BDBLVU>
2021-05-28 11:40:49 +0900

    회원가입 소스 병합

--- Team/src/main/java/deu/cse/team/source/SignUp.java
+++ Team/src/main/java/deu/cse/team/source/SignUp.java
@@ -59,7 +59,7 @@ public class SignUp implements FileInterface {
         for (int i = 0; i < readinfo.size(); i++) {
             line = readinfo.get(i);
             String[] str = line.split(" ");
-            signupinfo.add(new SignUpdataInfo(str[0],str[1],str[2],str[3]));
+            signupinfo.add(new SignUpdataInfo(str[0],str[1],str[2],str[3],str[4],str[5],str[6]));
         }
     }
     
