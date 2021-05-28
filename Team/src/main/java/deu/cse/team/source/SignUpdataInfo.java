f8b46622a680787e0fa59cae39b0d61e1e16e278
구본영 <@DESKTOP-1BDBLVU>
2021-05-28 11:40:00 +0900

    회원가입 소스 병합

--- Team/src/main/java/deu/cse/team/source/SignUpdataInfo.java
+++ Team/src/main/java/deu/cse/team/source/SignUpdataInfo.java
@@ -12,17 +12,24 @@ package deu.cse.team.source;
 public class SignUpdataInfo {
     private String id;
     private String pw;
+    private String pw_check;
     private String name;
+    private String email;
     private String phonenum;
-
+    private String birth;
+    private String residence;
     
     
     
-    public SignUpdataInfo(String id, String pw, String name, String phonenum){
+    public SignUpdataInfo(String id, String pw, String name, String email, String phonenum, String birth, String residence){
         this.id = id;
         this.pw = pw;
         this.name = name;
+        this.email = email;
         this.phonenum = phonenum;
+        this.birth = birth;
+        this.residence = residence;
+        
     }
 
     public String getId() {
@@ -56,4 +63,37 @@ public class SignUpdataInfo {
     public void setPhonenum(String phonenum) {
         this.phonenum = phonenum;
     }
+
+    public String getPw_check() {
+        return pw_check;
+    }
+
+    public void setPw_check(String pw_check) {
+        this.pw_check = pw_check;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getResidence() {
+        return residence;
+    }
+
+    public void setResidence(String region) {
+        this.residence = residence;
+    }
+    
 }
