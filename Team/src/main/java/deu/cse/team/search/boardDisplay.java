53551b5afe1430992f9b05f2a74317bc888f2c12
구본영 <@DESKTOP-1BDBLVU>
2021-05-28 11:48:23 +0900

    05/28 각 브런치 소스 병합

---
+++ Team/src/main/java/deu/cse/team/search/boardDisplay.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deu.cse.team.search;
+
+/**
+ * 기본 게시판 표시 클래스
+ * @author CHANG
+ */
+public class boardDisplay extends Display {
+    @Override
+    public void board() {
+        System.out.println("기본 게시판 내용 표시");
+    }
+}
